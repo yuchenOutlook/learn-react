a1b6428d7efde75ca5e451780233f3831884df81
VincentXiao <xiaoyuchen58@outlook.com>
2024-04-15 10:11:40 +0800

    first commit for course material.

---
+++ 02-javascript-review/script.js
@@ -0,0 +1,225 @@
+const data = [
+  {
+    id: 1,
+    title: "The Lord of the Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: [
+      "fantasy",
+      "high-fantasy",
+      "adventure",
+      "fiction",
+      "novels",
+      "literature",
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: "El señor de los anillos",
+      chinese: "魔戒",
+      french: "Le Seigneur des anneaux",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
+      },
+    },
+  },
+  {
+    id: 2,
+    title: "The Cyberiad",
+    publicationDate: "1965-01-01",
+    author: "Stanislaw Lem",
+    genres: [
+      "science fiction",
+      "humor",
+      "speculative fiction",
+      "short stories",
+      "fantasy",
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
+      },
+    },
+  },
+  {
+    id: 3,
+    title: "Dune",
+    publicationDate: "1965-01-01",
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: "",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+  },
+  {
+    id: 4,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: "1997-06-26",
+    author: "J. K. Rowling",
+    genres: ["fantasy", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: "Harry Potter y la piedra filosofal",
+      korean: "해리 포터와 마법사의 돌",
+      bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+      portuguese: "Harry Potter e a Pedra Filosofal",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
+      },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
+      },
+    },
+  },
+  {
+    id: 5,
+    title: "A Game of Thrones",
+    publicationDate: "1996-08-01",
+    author: "George R. R. Martin",
+    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: "왕좌의 게임",
+      polish: "Gra o tron",
+      portuguese: "A Guerra dos Tronos",
+      spanish: "Juego de tronos",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
+      },
+    },
+  },
+];
+
+function getBooks() {
+  return data;
+}
+
+function getBook(id) {
+  return data.find((d) => d.id === id);
+}
+
+const books = getBooks();
+books;
+
+// destructuring
+const book = getBook(2);
+book;
+const title = book.title;
+
+// this is the actual data deconstruction, the properties are extracted from the object,
+// hereby the book object
+const { author, genres } = book;
+title;
+author;
+genres;
+
+// deconstruction of an array
+const [firstGenre, secondaryGenre, ...otherGenre] = genres;
+
+console.log(firstGenre, secondaryGenre, otherGenre);
+
+const newArray = [...otherGenre, "epic new fantasy"];
+console.log(newArray);
+
+// updating the object
+const updatedBook = {
+  ...book,
+  // adding a new property to the object
+  rottenTomatoes: 9.5,
+  // updating the date of publication
+  publicationDate: "2024-01-01",
+};
+console.log(updatedBook);
+
+// javascript template literals, a way to concatenate strings, the variables are enclosed in ${}
+const summary = `${title} is a Book by ${author} selling at $${
+  20 + 30
+} dollars, and it belongs to the genres of ${genres.join()}`;
+summary;
+
+// ternary operator
+const isBookGood = updatedBook.rottenTomatoes > 8 ? "Good" : "Bad";
+isBookGood;
+
+// normal function
+function getYear(str) {
+  return str.split("-")[0];
+}
+const getYearFuncResult = getYear(updatedBook.publicationDate);
+getYearFuncResult;
+
+// lambda function
+const getYearLambda = (str) => str.split("-")[0];
+const year_lambda_result = getYearLambda(updatedBook.publicationDate);
+console.log(year_lambda_result);
+
+// short circuiting and logical operators
+// if the translations object is empty, the hasTranslations will be false
+const hasTranslations =
+  updatedBook.translations && Object.keys(updatedBook.translations).length > 0;
+console.log(hasTranslations);
+
+// falsy values in javascript
+// 0, null, undefined, NaN, "", false
+const falsyValues = [0, null, undefined, NaN, "", false];
+falsyValues.forEach((value) => {
+  console.log(Boolean(value));
+});
+
+const translations = updatedBook.translations.spanish || "No translation";
+console.log(translations);
+
+// nullish coalescing operator, the "??" operator will only
+// return the right side value if the left side is null or undefined or falsy values
+const wrongReviewCount =
+  updatedBook.reviews.librarything.reviewsCount || "No Reviews Found";
+console.log(wrongReviewCount);
+
+const rightReviewCount =
+  updatedBook.reviews.librarything.reviewsCount ?? "No Reviews Found";
+console.log(rightReviewCount);
