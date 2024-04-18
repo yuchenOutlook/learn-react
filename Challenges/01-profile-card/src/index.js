bf81f21fb722d40d0b2b6c72c4613b4ee24b8412
VincentXiao <xiaoyuchen58@outlook.com>
2024-04-18 11:42:58 +0800

    my own version of profile card built.

--- Challenges/01-profile-card/src/index.js
+++ Challenges/01-profile-card/src/index.js
@@ -5,14 +5,60 @@ import "./styles.css";
 function App() {
   return (
     <div className="card">
-      <Avatar />
+      <Avatar
+        className="avatar"
+        photoName="avatar/my_avatar.jpg"
+        name="Vincent Xiao"
+      />
       <div className="data">
-        <Intro />
+        <Intro
+          name="Vincent Xiao"
+          title="Senior SRE"
+          bio="Experienced full stack web developer with proficient work experience in SRE in startups."
+        />
         {/* Should contain one Skill component
         for each web dev skill that you have,
         customized with props */}
         <SkillList />
       </div>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div>
+      <Skill name="HTML+CSS" icon="/icons/example-icon.png"></Skill>
+      <Skill name="JavaScript" icon="/icons/example-icon.png"></Skill>
+      <Skill name="Web Dev" icon="/icons/example-icon.png"></Skill>
+      <Skill name="SRE" icon="/icons/example-icon.png"></Skill>
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill">
+      <h3>{props.name}</h3>
+      <img src={props.icon} alt={props.name}></img>
+    </div>
+  );
+}
+
+function Intro(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <p>{props.title}</p>
+      <p>{props.bio}</p>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return (
+    <div>
+      <img className="avatar" src={props.photoName} alt="{props.name}"></img>
     </div>
   );
 }
