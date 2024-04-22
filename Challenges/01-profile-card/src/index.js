5deaf5894f4c19d368193ce8e863a8d49cab06be
VincentXiao <xiaoyuchen58@outlook.com>
2024-04-22 16:04:00 +0800

    Refine the profile card.

--- Challenges/01-profile-card/src/index.js
+++ Challenges/01-profile-card/src/index.js
@@ -2,6 +2,69 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
+const skillList = [
+  {
+    skill: "Git",
+    level: "Advanced",
+    color: "#51e2f5",
+  },
+  {
+    skill: "C#",
+    level: "Advanced",
+    color: "#9df9ef",
+  },
+  {
+    skill: "ReactJS",
+    level: "Advanced",
+    color: "#fff685",
+  },
+  {
+    skill: "PostgreSQL",
+    level: "Advanced",
+    color: "#a0d2eb",
+  },
+  {
+    skill: "Python",
+    level: "Intermediate",
+    color: "#f5a2e5",
+  },
+  {
+    skill: "AWS",
+    level: "Intermediate",
+    color: "#f5a2e5",
+  },
+  {
+    skill: "CI/CD",
+    level: "Advanced",
+    color: "#a0d2eb",
+  },
+  {
+    skill: "GCP",
+    level: "Advanced",
+    color: "#fff685",
+  },
+  {
+    skill: "Kubernetes",
+    level: "Advanced",
+    color: "#9df9ef",
+  },
+  {
+    skill: "Docker",
+    level: "Advanced",
+    color: "#51e2f5",
+  },
+  {
+    skill: "Linux",
+    level: "Advanced",
+    color: "#f5a2e5",
+  },
+  {
+    skill: "Terrform",
+    level: "Intermediate",
+    color: "#f5a2e5",
+  },
+];
+
 function App() {
   return (
     <div className="card">
@@ -25,20 +88,29 @@ function SkillList() {
       <Skill name="JavaScript" icon="/icons/example-icon.png"></Skill>
       <Skill name="Web Dev" icon="/icons/example-icon.png"></Skill>
       <Skill name="SRE" icon="/icons/example-icon.png"></Skill> */}
-      <Skill name="HTML+CSS" emoji="💪🏻" color="#51e2f5"></Skill>
+      {skillList.map((skill) => (
+        <Skill
+          skill={skill.skill}
+          level={skill.level}
+          color={skill.color}
+          emoji={skill.level === "Advanced" ? "💪🏻" : "😉"}
+        />
+      ))}
+      {/* <Skill name="HTML+CSS" emoji="💪🏻" color="#51e2f5"></Skill>
       <Skill name="JavaScript" emoji="💪🏻" color="#9df9ef"></Skill>
       <Skill name="Web Dev" emoji="💪🏻" color="#fff685"></Skill>
-      <Skill name="SRE" emoji="😉" color="#a0d2eb"></Skill>
+      <Skill name="SRE" emoji="😉" color="#a0d2eb"></Skill> */}
     </div>
   );
 }
 
-function Skill(props) {
+function Skill({ skill, level, color, emoji }) {
   return (
     // You can just chain an image like this.
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.name}</span>
-      <span>{props.emoji}</span>
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      {/* <span>{level}</span> */}
+      <span>{emoji}</span>
     </div>
   );
 }
