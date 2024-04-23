d30e1310b51b893baf70c10513f3ef624d8abe8f
VincentXiao <xiaoyuchen58@outlook.com>
2024-04-23 10:37:32 +0800

    The best practices to update the current state.

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -15,18 +15,18 @@ export default function App() {
 
   function handlePrevious() {
     if (step > 1) {
-      setStep(step - 1);
+      setStep((s) => s - 1);
     }
   }
 
   function handleNext() {
     if (step < 3) {
-      setStep(step + 1);
+      setStep((s) => s + 1);
     }
   }
 
   function handleIsOpenState() {
-    setIsOpen(!isOpen);
+    setIsOpen((s) => !s);
   }
 
   return (
