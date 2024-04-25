dec990295f35946064c3d5a27ef27a7ec0d28417
VincentXiao <xiaoyuchen58@outlook.com>
2024-04-25 12:56:32 +0800

    - Create Travel-list project - Divide the components - build lay-out - render item list - handle form submittion

---
+++ 05-travel-list/src/App.js
@@ -0,0 +1,71 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "iPad", quantity: 1, packed: true },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1 className="">🏝️FAR AWAY🥂</h1>;
+}
+
+function Form() {
+  function submitHandler(event) {
+    event.preventDefault();
+  }
+  return (
+    <div className="add-form" onSubmit={submitHandler}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>ADD</button>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item}></Item>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? {} : { textDecoration: "Line-through" }}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      You have {} items on your list, and you already packed 0 {}%
+    </footer>
+  );
+}
