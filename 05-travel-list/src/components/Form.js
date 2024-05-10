8d6800b8517071b240a44c03b33e4eb0897abaa9
VincentXiao <xiaoyuchen58@outlook.com>
2024-05-10 14:31:11 +0800

    Separate all the component files, clean the project hierachy. Complete the project.

---
+++ 05-travel-list/src/components/Form.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: true, id: Date.now() };
+
+    onAddItems(newItem);
+    // set description and quantity to the initial state
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>ADD</button>
+    </form>
+  );
+}
---
+++ 05-travel-list/src/components/Stats.js
@@ -0,0 +1,22 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your list! 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => !item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything ready! 🎉"
+          : `
+          You have ${numItems} items on your list, and you already packed
+          ${numPacked} items. That's ${percentage}%!`}
+      </em>
+    </footer>
+  );
+}
