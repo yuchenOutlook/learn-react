a55155d203714c783616fcf779b7940dfa47eb4f
VincentXiao <xiaoyuchen58@outlook.com>
2024-05-11 13:12:15 +0800

    Complete the accordion exercise with state isOpen and a handler.

--- Exercises/accordion-component/src/App.js
+++ Exercises/accordion-component/src/App.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 
+import { useState } from "react";
+
 const faqs = [
   {
     title: "Where are these chairs assembled?",
@@ -27,19 +29,30 @@ function Accordion({ data }) {
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i}></AccordionItem>
+        <AccordionItem
+          title={el.title}
+          text={el.text}
+          num={i}
+          key={el.title}
+        ></AccordionItem>
       ))}
     </div>
   );
 }
 
 function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
   return (
-    <div className="item">
-      <p className="number">{num}</p>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : `${num + 1}`}</p>
       <p className="title">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
