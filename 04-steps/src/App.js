ab50b893db55de0bfbe36d782a872d25db6421e7
VincentXiao <xiaoyuchen58@outlook.com>
2024-05-11 14:44:22 +0800

    add more examples for children props.

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -41,9 +41,10 @@ export default function App() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
+          {/* <p className="message">
             Step {step} : {messages[step - 1]}
-          </p>
+          </p> */}
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
             <Button
               bgColor="#7950f2"
@@ -70,5 +71,14 @@ function Button({ textColor, bgColor, onClick, children }) {
     >
       {children}
     </button>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   );
 }
