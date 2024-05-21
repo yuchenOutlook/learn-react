4a94b0b7a98b5bc0ef39068227c1509e18af88d7
VincentXiao <xiaoyuchen58@outlook.com>
2024-05-21 14:38:00 +0800

    Added starRating component in separate file, added handler and states, the star image and styles.

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StartRating from "./StartRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StartRating maxRating={5} />
+    <StartRating maxRating={10} />
+    <StartRating />
   </React.StrictMode>
 );
