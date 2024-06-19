984c14e93e51d99a1352b517ddfbc05df0f25fe4
VincentXiao <xiaoyuchen58@outlook.com>
2024-06-19 17:13:02 +0800

    Finish up on section 11.

--- 08-how-react-works/src/App.js
+++ 08-how-react-works/src/App.js
@@ -69,6 +69,12 @@ function TabContent({ item }) {
     setLikes(likes + 1);
   }
 
+  function handleTripleInc() {
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -82,7 +88,7 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
