a6ea6b0229932d6949596bf0e6573bb86439051f
VincentXiao <xiaoyuchen58@outlook.com>
2024-06-26 15:26:28 +0800

    Challenge added to convert the currency.

--- Challenges/05-currency-converter/src/App.js
+++ Challenges/05-currency-converter/src/App.js
@@ -6,11 +6,12 @@ export default function App() {
   const [fromCur, setFromCur] = useState("EUR");
   const [toCur, setToCur] = useState("USD");
   const [converted, setConverted] = useState("");
-
+  const [isLoading, setisLoading] = useState(false);
   useEffect(
     function () {
       async function convertCurrency() {
         try {
+          setisLoading(true);
           const res = await fetch(
             `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
           );
@@ -23,19 +24,20 @@ export default function App() {
             throw new Error("Movie not found.");
           }
           setConverted(data.rates[toCur]);
+          setisLoading(false);
         } catch (error) {
           console.log("log error");
         } finally {
         }
       }
+
+      if (fromCur === toCur) {
+        return setConverted(amount);
+      }
       convertCurrency();
     },
     [amount, fromCur, toCur]
   );
-
-  function handleCurrentcySelection() {
-    console.log("currency");
-  }
 
   return (
     <div>
@@ -43,14 +45,17 @@ export default function App() {
         type="text"
         value={amount}
         onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
       />
       <CurrencySelect
         value={fromCur}
         onChange={(e) => setFromCur(e.target.value)}
+        isLoading={isLoading}
       ></CurrencySelect>
       <CurrencySelect
         value={toCur}
         onChange={(e) => setToCur(e.target.value)}
+        isLoading={isLoading}
       ></CurrencySelect>
       <p>
         {converted} {toCur}
@@ -59,10 +64,10 @@ export default function App() {
   );
 }
 
-function CurrencySelect({ value, onChange }) {
+function CurrencySelect({ value, isLoading, onChange }) {
   return (
     <>
-      <select onChange={onChange}>
+      <select onChange={onChange} disabled={isLoading}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
